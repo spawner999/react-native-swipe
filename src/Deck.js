71748ef91cb6a0af1b2c77de98a77b5698da4620
Lorenzo <lferrario@byte-code.com>
2017-05-11 15:58:32 +0200

    Section 3: PanResponder

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { View } from 'react-native';
+import { Animated, PanResponder } from 'react-native';
 
 class Deck extends Component {
   static propTypes = {
@@ -13,16 +13,38 @@ class Deck extends Component {
     renderCard: PropTypes.func.isRequired
   }
 
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        const { dx, dy } = gesture;
+        // HACK this is updating state outside of setState()
+        position.setValue({ x: dx, y: dy });
+      },
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder, position };
+  }
+
   renderCards() {
     const { data, renderCard } = this.props;
     return data.map(i => (renderCard(i)));
   }
 
   render() {
+    const { panResponder: { panHandlers }, position } = this.state;
+
     return (
-      <View>
+      <Animated.View
+        style={position.getLayout()}
+        {...panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
     );
   }
 }
